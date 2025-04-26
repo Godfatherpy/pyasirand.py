84b81cdbee4fb2ca8c82fd73e2ae4762a3004395
Godfatherpy <fschool749@gmail.com>
2025-04-26 17:48:27 +0530

    Update __init__.py

--- handlers/__init__.py
+++ handlers/__init__.py
@@ -1,1 +1,15 @@
+# handlers/__init__.py
+
+from .user import (
+    start_command,
+    get_video_command,
+    navigation_callback,
+    category_callback,
+)
+
+from .admin import (
+    add_category_command,
+    remove_category_command,
+    admin_callback,
+)
 
