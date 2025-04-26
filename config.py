23c212bd5a8af126d90069e2b003ab024918c1c3
Godfatherpy <fschool749@gmail.com>
2025-04-26 20:11:00 +0530

    Update config.py

--- config.py
+++ config.py
@@ -6,10 +6,10 @@ from dotenv import load_dotenv
 load_dotenv()
 
 TELEGRAM_BOT_TOKEN = os.getenv("7646433933:AAFNt3yLxXuwOPhJEfGyFtvNgkljQgJx-Wk")
-MONGODB_URI = os.getenv("MONGODB_URI")
+MONGODB_URI = os.getenv("mongodb+srv://babuhaiker:<pOstGjwAZDVYDSNK>@cluster0.hycmfqe.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
 
 # Optional: Admin user IDs (comma-separated in .env)
-ADMIN_IDS = [int(uid) for uid in os.getenv("ADMIN_IDS", "").split(",") if uid.strip()]
+ADMIN_IDS = [int(uid) for uid in os.getenv("1920026281", "").split(",") if uid.strip()]
 
 # Optional: Default settings
 DEFAULT_CATEGORY = os.getenv("DEFAULT_CATEGORY", "general")
@@ -20,4 +20,4 @@ if not TELEGRAM_BOT_TOKEN or not MONGODB_URI:
   
 # config.py
 
-URL_SHORTENER_API = "https://your-shortener-service.com/api?url={}"
+URL_SHORTENER_API = "https://easysky.in/st?api=c140f4465e38cdee50c2694418c299a3ca5b25db&url=yourdestinationlink.com"
