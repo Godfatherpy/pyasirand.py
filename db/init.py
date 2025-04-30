00977ce1b9e143e79eea2fb35203b8f0419fd395
Godfatherpy <fschool749@gmail.com>
2025-05-01 03:33:34 +0530

    Update init.py

--- db/init.py
+++ db/init.py
@@ -1,6 +1,8 @@
 from pymongo import MongoClient
 
-def init_db(mongo_uri: str):
+def init_db(mongo_uri: str, db_name: str = None):
     client = MongoClient(mongo_uri)
-    return client
-  
+    if db_name:
+        return client[db_name]
+    else:
+        return client.get_default_database()
