ae072f0eab553874771c1f1cab30802a721affc8
Godfatherpy <fschool749@gmail.com>
2025-05-01 03:11:07 +0530

    Update config.py

--- config.py
+++ config.py
@@ -5,6 +5,7 @@ load_dotenv()
 
 TELEGRAM_BOT_TOKEN = os.getenv("TELEGRAM_BOT_TOKEN")
 MONGODB_URI = os.getenv("MONGODB_URI")
+URL_SHORTENER_API = os.getenv("URL_SHORTENER_API")
 
-if not TELEGRAM_BOT_TOKEN or not MONGODB_URI:
+if not TELEGRAM_BOT_TOKEN or not MONGODB_URI or not URL_SHORTENER_API:
     raise ValueError("Missing required environment variables")
